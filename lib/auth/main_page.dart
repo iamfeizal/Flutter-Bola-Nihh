07117ca3ff22ef3e05be6e50d292470678759e21
Imam Agus Faisal <81522796+iamfeizal@users.noreply.github.com>
2022-11-30 09:24:05 +0700

    fixing bug

--- lib/auth/main_page.dart
+++ lib/auth/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:final_project_api/page/login_page.dart';
 import 'package:final_project_api/page/home_page.dart';
 
 import 'auth_page.dart';
